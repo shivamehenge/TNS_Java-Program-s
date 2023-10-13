9ac3759f2b6b37c2855c864f7e812b5fc0d4af34
Shiva Mehenge <121855486+shivamehenge@users.noreply.github.com>
2023-10-13 13:04:28 +0530

    Java

---
+++ smallNumber.java
@@ -0,0 +1,28 @@
+
+public class smallNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 10;
+		int c = 20;
+
+		if (a<b&&a<c)
+		{
+			System.out.println("a is smaller!");
+
+		}
+		if(b<a&&b<c)
+		{
+			System.out.println("b is smaller!");
+		}
+
+		if(c<a&&c<b)
+		{
+			System.out.println("c is smaller!");
+
+	    }
+
+	}
+
+}
